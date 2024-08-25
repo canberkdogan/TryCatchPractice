0d4828eecc663f16996c492b66f0c7f2b946a783
canbe <canbe@CANBERK>
2024-08-25 23:22:46 +0300

    Add project files.

---
+++ TryCatchPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Please enter number---->");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int square = number * number;
+
+    Console.WriteLine($"your numbers square is ---->{square}");
+
+
+}
+catch
+{
+    Console.WriteLine("you are enter wrong item. Please try again.");
+}
